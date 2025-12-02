25212fa72a8b4edf3df32006533174809cd8305c
NITIN KIROLA <nitinkirola1701@gmail.com>
2025-12-03 04:39:29 +0530

    feat: zod validation

---
+++ schema/auth.ts
@@ -0,0 +1,12 @@
+import { email, z } from "zod";
+
+export const RegisterSchema = z.object({
+  email: z.string().min(4).max(30),
+  username: z.string().min(4).max(20),
+  password: z.string().min(3).max(100)
+})
+
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(100),
+});
