68df0b56b14db279c29980b1cd05c678d165ba4f
NITIN KIROLA <nitinkirola1701@gmail.com>
2025-12-04 10:44:34 +0530

    feat: Register route created

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { prisma } from "@/lib/prisma";
+import { RegisterSchema } from "@/schema/auth";
+import { error } from "console";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const parsed = RegisterSchema.safeParse(body);
+    
+    if (!parsed.success) {
+      return NextResponse.json({ message: "Invalid Input" }, { status: 400 });
+    }
+
+    const { email, password, username } = parsed.data;
+    //existing already
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { username }],
+      },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User Already Exist" },
+        { status: 400 }
+      );
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        username,
+        password: hashed,
+      },
+    });
+
+    return NextResponse.json({ user });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ error: "Server Error" }, { status: 500 });
+  }
+}
